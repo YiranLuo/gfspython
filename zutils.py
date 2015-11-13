4742f63572bfd0ac87ddd7ab40130b2593473225
Shane McCulley <smcculle@uno.edu>
2015-11-13 15:35:05 -0600

    some net stats

--- zutils.py
+++ zutils.py
@@ -35,7 +35,15 @@ def get_client():
     return client
 
 
-
+# def get_mem(servername):
+    # res = os.popen('ssh %s "grep MemFree /proc/meminfo | sed \'s/[^0-9]//g\'"' % servername)
+    # return res.read().strip()
+    # vnstat -i eth0 -tr | grep rx | awk '{print "RX rate: " $2 " " $3}'
+    #  RX rate: 62.18 kB/s
+    # vnstat -i eth0 -tr | grep tx | awk '{print "TX rate: " $2 " " $3}'
+    #  TX rate: 120.54 kB/s
+    # vnstat takes too long
+    # ifstat -q -i wlan0 -S 0.1 1 | perl -n -e '/(\d+\.\d+).*(\d+\.\d+)/ && print "Down: $1 KBps - Up: $2 KBps\n"'
 
 
 
