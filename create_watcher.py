58196d1499d0d5f61049951fa0d3eb242ac72dea
Shane McCulley <smcculle@uno.edu>
2018-03-21 17:35:13 -0500

    Pylint cleanup

--- create_watcher.py
+++ create_watcher.py
@@ -1,41 +1,54 @@
-import sys
+""" Creates the Master server for OFS that handles metadata and directs clients/chunkservers.
+
+This module creates an instance of `zmaster.ZMaster` and registers with Zookeeper.  After registration,
+the master queries all chunkservers registered with zookeeper for their file contents and metadata.
+
+Example:
+
+$ python create_watcher.py -p 1401 -z localhost:2121
+
+Todo:
+    * Consolidate into a single create script that accepts master/server/watcher as an operand
+"""
+
+import argparse
+import logging
 
 from zmq import ZMQError
 import zerorpc
 
-import zutils
+
 import watcher
-
-PORT = '1401'
-ZOO_IP = 'localhost'
+import zutils
 
 
-def main(argv):
-    if argv:
-        zoo_ip = str(argv[0])
-    else:
-        zoo_ip = ZOO_IP
+def main():
+    """Parse args and start the server"""
+    logger = logging.getLogger(__name__)
+    parser = argparse.ArgumentParser(description='Start chunkserver')
+    parser.add_argument('-p', '--port', default=1401, type=int, help='Base port to start from, default 4400. '
+                                                                     'Chunkserver registration number added to base.')
+    parser.add_argument('-z', '--zoo-ip', dest='zoo_ip', default='localhost', type=str, help='IP address of zookeeper')
 
-    chunkserver = watcher.Watcher(zoo_ip=zoo_ip, port=PORT)
+    args = parser.parse_args()
+    shadow = watcher.Watcher(zoo_ip=args.zoo_ip, port=args.port)
+    address = f'tcp://{zutils.get_myip()}:{args.port}'
 
-    s = zerorpc.Server(chunkserver)
-
-    address = 'tcp://%s:%s' % (zutils.get_myip(), PORT)
+    serv = zerorpc.Server(shadow)
+    serv.bind(address)
+    logger.info(f'Registered shadow master at {address}')
 
     try:
-        print 'Registering watcher at %s' % address
-        s.bind(address)
-        s.run()
-    except ZMQError as e:
-        print "Unable to start watcher: " + e.strerror
-        s.close()
-        sys.exit(2)
+        serv.run()
+    except ZMQError as err:
+        logging.exception(err.strerror)
+        raise SystemExit('Unable to start server due to exception:  {e.strerror}')
     except KeyboardInterrupt:
         pass
     finally:
-        print 'Closing watcher on %s' % address
-        s.close()
+        logger.info(f'Closing shadow master at {address}')
+        serv.close()
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
