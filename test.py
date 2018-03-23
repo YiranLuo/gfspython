57858ca79ead06fa7cf462721c80fcdeb7c6c748
Shane McCulley <smcculle@uno.edu>
2018-03-22 23:16:07 -0500

    Code cleanup, linting fixes

--- test.py
+++ test.py
@@ -1,55 +1,64 @@
-import zclient
+""" Test multiple writing, reading, and deleting actions randomly to determine server stability """
 import random
 
-client=zclient.ZClient()
+import zclient
 
-#flag=raw_input('Enter "q" to quit- ')
-flag=1
-files=['file1.txt','file2.txt','file3.txt','file4.txt','file5.txt']
-readwhen=[1,4,7,9]
-writewhen=[0,2,3,5,8]
-deletewhen=[6]
 
-choice=0
+def main():
+    """ Execute test """
+    client = zclient.ZClient()
 
-while flag!= '0':
-  a=client.master.get('filetable').keys()
-  a.remove('#garbage_collection#')
+    # flag=raw_input('Enter 'q' to quit- ')
+    flag = 1
+    files = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt']
+    readwhen = [1, 4, 7, 9]
+    writewhen = [0, 2, 3, 5, 8]
+    deletewhen = [6]
 
-  print "a =",a
+    choice = 0
 
-  #read file
-  if a!=[] and choice in readwhen:
-   filename=random.choice(a)
-   print "read ",filename
-   client.read(filename)
-   print "---"
+    while flag != '0':
+        filetable = list(client.master.get('filetable').keys())
+        filetable.remove('#garbage_collection#')
 
-  #write file
-  if choice in writewhen:
-    filename=random.choice(files)
-    with open("upload/"+filename,'r') as f:
-      data=f.read()
-    
-    if random.getrandbits(1) and a!=[]:
-      print "edit ",writefname,filename
-      writefname=random.choice(a) #edit
-    else:
-      print "write ",writefname,filename
-      writefname=str(random.random()).split('.')[1]+".txt" #write
+        print(f'filetable = {filetable}')
 
-    client.write(writefname, data)
-    print "---"
+        # read file
+        if filetable != [] and choice in readwhen:
+            filename = random.choice(filetable)
+            print(f'read {filename}')
+            client.read(filename)
+            print('---')
 
-  #delete file
-  if a!=[] and choice in deletewhen:
-    filename=random.choice(a)
-    print "delete ",filename
-    client.delete(filename)
-    print "---"
-  
-  choice=random.randint(0,9)
+        # write file
+        if choice in writewhen:
+            filename = random.choice(files)
+            with open('upload/' + filename, 'r') as f:
+                data = f.read()
 
-  with open('upload/flag.txt','r') as f:
-    flag=f.read().strip('\n')
-  
+            if random.getrandbits(1) and filetable != []:
+                writefname = random.choice(filetable)  # edit
+                print(f'edit {writefname} {filename}')
+
+            else:
+                writefname = str(random.random()).split('.')[1] + '.txt'  # write
+                print(f'edit {writefname} {filename}')
+
+            client.write(writefname, data)
+            print('---')
+
+        # delete file
+        if filetable != [] and choice in deletewhen:
+            filename = random.choice(filetable)
+            print(f'delete {filename}')
+            client.delete(filename)
+            print('---')
+
+        choice = random.randint(0, 9)
+
+        with open('upload/flag.txt', 'r') as f:
+            flag = f.read().strip('\n')
+
+
+if __name__ == '__main__':
+    main()
