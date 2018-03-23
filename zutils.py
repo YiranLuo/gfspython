5093e1114f81f25148ffbc293d0aae24fb2946f2
Shane McCulley <smcculle@uno.edu>
2018-03-23 13:17:21 -0500

    Add docstrings, f-strings, and logging

--- zutils.py
+++ zutils.py
@@ -1,49 +1,56 @@
 """A list of utility functions for the classes and functions for easy creation
 of test objects like zookeeper client, regular client"""
 
+import logging
+
 
 def get_myip():
+    """ Returns IP address.
+    ref https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
+    """
     import socket
-    """ Returns IP address"""
-    return [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+    return [l for l in (
+        [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [
+            [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in
+             [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
 
 
-def get_tcp(port=None):
-    if port:
-        return "tcp://%s:%d" % (get_myip(), port)
-    else:
-        return 'tcp://%s' % get_myip()
+def get_tcp(port=''):
+    """ Return a tcp protocol string using local ip with optional port added"""
+    port_string = ':{}'
+    return f'tcp://{get_myip()}{port_string.format(port) if port else ""}'
 
 
-def get_zk(ip='localhost:2181'):
+def get_zk(addr='localhost:2181'):
+    """
+    Starts a `KazooClient` to interface with the Zookeeper server.
+    :param addr: IP and port of the Zookeeper service.
+    :return: `KazooClient` object connected to the Zookeeper service.
+    """
     from kazoo.client import KazooClient
     from kazoo.handlers.threading import KazooTimeoutError
 
-    zk = KazooClient(hosts=ip)
+    zookeeper = KazooClient(hosts=addr)
     try:
-        zk.start(timeout=10)
+        zookeeper.start(timeout=0)
     except KazooTimeoutError:
-        print "Couldn't connect to zookeeper"
-        zk.stop()
+        logging.getLogger(__name__).critical('Could not connect to zookeeper')
+        raise
 
-    return zk
+    return zookeeper
 
 
 def get_client():
+    """ Start and return a `ZClient` object"""
     from zclient import ZClient
-    client = ZClient()
-    return client
-
+    return ZClient()
 
 # def get_mem(servername):
-    # res = os.popen('ssh %s "grep MemFree /proc/meminfo | sed \'s/[^0-9]//g\'"' % servername)
-    # return res.read().strip()
-    # vnstat -i eth0 -tr | grep rx | awk '{print "RX rate: " $2 " " $3}'
-    #  RX rate: 62.18 kB/s
-    # vnstat -i eth0 -tr | grep tx | awk '{print "TX rate: " $2 " " $3}'
-    #  TX rate: 120.54 kB/s
-    # vnstat takes too long
-    # ifstat -q -i wlan0 -S 0.1 1 | perl -n -e '/(\d+\.\d+).*(\d+\.\d+)/ && print "Down: $1 KBps - Up: $2 KBps\n"'
-
-
-
+# res = os.popen('ssh %s "grep MemFree /proc/meminfo | sed \'s/[^0-9]//g\'"' % servername)
+# return res.read().strip()
+# vnstat -i eth0 -tr | grep rx | awk '{print "RX rate: " $2 " " $3}'
+#  RX rate: 62.18 kB/s
+# vnstat -i eth0 -tr | grep tx | awk '{print "TX rate: " $2 " " $3}'
+#  TX rate: 120.54 kB/s
+# vnstat takes too long
+# ifstat -q -i wlan0 -S 0.1 1 | perl -n -e '/(\d+\.\d+).*(\d+\.\d+)/ && print "Down: $1 KBps - Up: $2 KBps\n"'
